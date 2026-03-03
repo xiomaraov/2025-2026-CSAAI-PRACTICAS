263086c61bdb7a95190597886cd95be80f1b51cf
xiomarao <x.osorio.2023@alumnos.urjc.es>
2026-03-03 10:25:33 +0100

    teoria 3 marzo

---
+++ P2/Javasript/ejemplo_3.js
@@ -0,0 +1,7 @@
+// Esto son comentarios en Javascript
+// MI primer programa javascript!!
+
+//-- Imprimir tres mensajes en la consola
+console.log("Estoy fuera del HTML");
+console.log("Se recomienda hacerlo así");
+console.log("el jueves tenemos examen de javascript");
